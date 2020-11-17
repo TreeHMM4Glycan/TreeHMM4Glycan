432f0a9aef560b456eddf2a5864d2422ed892731
Fei Wang <tellfei@hotmail.com>
2020-11-16 23:52:51 -0700

    some update and let us call for the day

--- treehmm4glycan.py
+++ treehmm4glycan.py
@@ -19,35 +19,23 @@ def get_iupcas(iupac_name_file):
                 iupacs[id] = iupac
     return iupacs
 
-def example1(glycan:Glycan, number_state = 2):
+def create_and_run_treehmm(glycan:Glycan, number_state = 2, emissions = None):
     sample_tree = glycan.get_adj_matrix()
-    emissions = glycan.get_emssions()
-
-    states = ['P','N']
-    #state_transition_probabilities = np.array([0.1,0.9,0.1,0.9]).reshape(2,2)
-    hmm = initHMM.initHMM(states,emissions,sample_tree,state_transition_probabilities = state_transition_probabilities)
 
     # Declaring the emission_observation list
-    emission_observation = [["L","M","H","M","L","L"],["M","L","H","H","L","L"]]
+    emission_observation = glycan.get_emssions()
 
-    # Declaring the observed_states_training_nodes
-    data = {'node' : [0,3,4], 'state' : ['P','N','P']}
-    observed_states_training_nodes = pd.DataFrame(data = data,columns=["node","state"])
+    #states = ['P','N']
+    # create states
+    states = [ str(i) for i in range(number_state)]
+    #state_transition_probabilities = np.array([0.1,0.9,0.1,0.9]).reshape(2,2)
+    hmm = initHMM.initHMM(states, emissions, sample_tree)
 
-    # Declaring the observed_states_validation_nodes
-    data1 = {'node' : [1,2], 'state' : ['N','P']}
-    observed_states_validation_nodes = pd.DataFrame(data = data1,columns=["node","state"])
 
     # The baumWelch part: To find the new parameters and result statistics
-    newparam = baumWelch.baumWelchRecursion(copy.deepcopy(hmm), emission_observation, observed_states_training_nodes, observed_states_validation_nodes)
-    #learntHMM = baumWelch.baumWelch(copy.deepcopy(hmm), emission_observation, observed_states_training_nodes, observed_states_validation_nodes)
+    newparam = baumWelch.baumWelchRecursion(hmm, emission_observation)
 
-    print("newparam :", newparam)
-    print("\n")
-    #print("learntHMM : ", learntHMM)
-    #print("\n")
-
-def example2():
+def example1():
     sample_tree = np.array([0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0]).reshape(6,6)
     #sparse = csr_matrix(sample_tree)
     states = ['P','N']
@@ -85,5 +73,5 @@ def example2():
 
 if __name__ == "__main__":
     #pass
-    example2()
+    example1()
     #iupac_name_file = './Data/IUPAC.csv'
