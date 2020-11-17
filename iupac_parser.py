4c4bffe3bdec4fbed703d9b6e8f6e0fe4a669c04
Fei Wang <tellfei@hotmail.com>
2020-11-16 19:05:21 -0700

    change code to parse all inputs

--- iupac_parser.py
+++ iupac_parser.py
@@ -1,17 +1,19 @@
 from TreeHMM4Glycan.Glycan import Glycan
 
-case1 = '(3S)Gal(b1-4)[Fuc(a1-3)](6S)Glc'
-case2 = '6S(3S)Gal(b1-4)[Fuc(a1-3)][Fuc(a1-3)]Glc'
-case3 =  'Neu5Ac(a2-3)Gal(b1-4)GlcNAc(b1-3)Gal(b1-4)GlcNAc(b1-3)Gal(b1-4)GlcNAc(b1-2)Man(a1-6)[Neu5Ac(a2-3)Gal(b1-4)GlcNAc(b1-3)Gal(b1-4)GlcNAc(b1-3)Gal(b1-4)GlcNAc(b1-2)Man(a1-3)]Man(b1-4)GlcNAc(b1-4)GlcNAc'
-case4 = 'KDN(a2-3)Gal(b1-3)GlcNAc(a1-Sp0'
-#'GlcNAc(b1-6)[GlcNAc(b1-2)](3S)Man(a1-6)[GlcNAc(b1-4)][GlcNAc(b1-4)[GlcNAc(b1-2)]Man(a1-3)]Man'
-#glycan_from_iupac(case3)
-#case2_glycan = Glycan(case2)
-#print(case2_glycan.get_emssions())
-for case in [case1, case2, case3, case4]:
-    glycan = Glycan(case)
-    print(case)
-    print(glycan.get_emssions_with_linkage())
-    print(glycan.get_adj_list())
-    print(glycan.get_emssions())
-    print(glycan.get_adj_matrix())
+import csv
+
+if __name__ == "__main__":
+    iupac_name_file = './Data/IUPAC.csv'
+    with open(iupac_name_file) as file_in:
+        csv_reader = csv.reader(file_in)
+        for idx,row in enumerate(csv_reader):
+            if idx == 0:
+                continue
+            case = row[1]
+            count = int(row[2])
+            if count > 0:
+                glycan = Glycan(case)
+                print(case)
+                print(glycan.get_emssions_with_linkage())
+                print(glycan.get_adj_list())
+                #print(glycan.get_emssions())
