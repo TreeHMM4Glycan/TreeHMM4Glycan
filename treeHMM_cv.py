e06fa2b5bf2e109d1ff6a6c0a7510b8f4023dd54
Shiang Qi <shiang@ualberta.ca>
2020-11-22 11:47:38 -0700

    revise cross validation

--- treeHMM_cv.py
+++ treeHMM_cv.py
@@ -1,10 +1,14 @@
 import numpy as np
 import copy
+import re
 import pandas as pd
 from sklearn.model_selection import KFold
 from treehmm4glycan import create_forest_inputs, get_iupcas
 from TreeHMM4Glycan.Glycan import Glycan
-from treehmm import initHMM, baumWelch
+from treehmm import initHMM, baumWelch, forward, fwd_seq_gen
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, classification_report
+from scipy.sparse import csr_matrix
+from scipy.special import logsumexp
 
 
 def get_data():
@@ -49,9 +53,12 @@ def n_fold(data, col_names, protein, num_folds, seed = None):
     return binding_train, binding_test, nonbinding_train, nonbinding_test
 
 
-def train():
+def train(n_folds=10, n_states=5, max_iter=50, delta=1e-5, random_seed=None):
     """Train GlyNet using n-fold cross-validation.
     """
+    cv_label = []
+    cv_pred = []
+
     iupac_name_file = './Data/IUPAC.csv'
     iupacs = get_iupcas(iupac_name_file)
     gylcans = {}
@@ -72,53 +79,85 @@ def train():
             counts[mono_count] += 1
     emissions = list(set(monos))
     emissions = [emissions]
-    states = ['1', '2', '3', '4', '5']
+    states = [str(i) for i in range(1, n_states + 1)]
 
     data, col_names = get_data()
     target_protein = 'AAL (100 ug/ml)'
-    binding_train, binding_test, nonbinding_train, nonbinding_test = n_fold(data, col_names, target_protein, num_folds=10)
+    binding_train, binding_test, nonbinding_train, nonbinding_test = n_fold(data, col_names, target_protein,
+                                                                            num_folds=n_folds, seed=random_seed)
     for bind_train, bind_test, nonbind_train, nonbind_test in zip(binding_train, binding_test, nonbinding_train, nonbinding_test):
         # prepare glycan dictionary
         glycans_bind_train = {}
         glycans_nonbind_train = {}
         for i in range(len(bind_train)):
-            inpuac_text = bind_train[i]
-            glycan_bind_train = Glycan(inpuac_text)
+            iupac_text = bind_train[i]
+            iupac = re.split(r"\([^\)]*$", iupac_text, 1)[0]
+            glycan_bind_train = Glycan(iupac)
             glycans_bind_train[i] = glycan_bind_train
 
         for i in range(len(nonbind_train)):
-            inpuac_text = nonbind_train[i]
-            glycan_nonbind_train = Glycan(inpuac_text)
+            iupac_text = nonbind_train[i]
+            iupac = re.split(r"\([^\)]*$", iupac_text, 1)[0]
+            glycan_nonbind_train = Glycan(iupac)
             glycans_nonbind_train[i] = glycan_nonbind_train
 
         bind_adj_matrix, bind_mono_emission, bind_link_emission = create_forest_inputs(glycans_bind_train)
-        nonbind_adj_matrix, nonbind_mono_emission_obs, nonbind_link_emission = create_forest_inputs(glycans_nonbind_train)
+        nonbind_adj_matrix, nonbind_mono_emission, nonbind_link_emission = create_forest_inputs(glycans_nonbind_train)
+        bind_parse_matrix = csr_matrix(bind_adj_matrix)
+        nonbind_parse_matrix = csr_matrix(nonbind_adj_matrix)
 
-        binding_hmm = initHMM.initHMM(states, emissions, bind_adj_matrix)
-        nonbinding_hmm = initHMM.initHMM(states, emissions, nonbind_adj_matrix)
+        binding_hmm = initHMM.initHMM(states, emissions, random_init_state_transition_probabilities=True,
+                                      random_init_emission_probabilities=True)
+        nonbinding_hmm = initHMM.initHMM(states, emissions, random_init_state_transition_probabilities=True,
+                                      random_init_emission_probabilities=True)
 
         bind_mono_emission = [bind_mono_emission]
-        nonbind_mono_emission_obs = [bind_mono_emission]
+        nonbind_mono_emission = [nonbind_mono_emission]
 
-        bind_param = baumWelch.hmm_train_and_test(copy.deepcopy(binding_hmm), bind_mono_emission)
-        nonbind_param = baumWelch.hmm_train_and_test(copy.deepcopy(nonbinding_hmm), nonbind_mono_emission_obs)
+        bind_param = baumWelch.hmm_train_and_test(copy.deepcopy(binding_hmm), bind_parse_matrix, bind_mono_emission,
+                                                  maxIterations=max_iter, delta=delta)
+        nonbind_param = baumWelch.hmm_train_and_test(copy.deepcopy(nonbinding_hmm), nonbind_parse_matrix, nonbind_mono_emission,
+                                                     maxIterations=max_iter, delta=delta)
 
-        # needs to be changed here
-        binding_hmm_trained = initHMM.initHMM(states, emissions, )
-        nonbinding_hmm_trained = initHMM.initHMM(states, emissions, )
+        binding_hmm_trained = initHMM.initHMM(states, emissions, state_transition_probabilities=bind_param['hmm']['state_transition_probabilities'],
+                                              emission_probabilities=bind_param['hmm']['emission_probabilities'])
+        nonbinding_hmm_trained = initHMM.initHMM(states, emissions, state_transition_probabilities=nonbind_param['hmm']['state_transition_probabilities'],
+                                                 emission_probabilities=nonbind_param['hmm']['emission_probabilities'])
 
         testset = bind_test + nonbind_test
+        testlabel = [1] * len(bind_test) + [0] * len(nonbind_test)
+        testpred = []
         for i in range(len(testset)):
-            inpuac_text = testset[i]
-            glycan_test = Glycan(inpuac_text)
+            iupac_text = testset[i]
+            iupac = re.split(r"\([^\)]*$", iupac_text, 1)[0]
+            glycan_test = Glycan(iupac)
 
+            # get test data features
             glycan_adj_matrix = glycan_test.get_adj_matrix()
+            glycan_sparse_matrix = csr_matrix(glycan_adj_matrix)
             glycan_mono_emission = glycan_test.get_monosaccharide_emssions()
-            glycan_mono_emission = glycan_test.get_linkage_emssions()
+            glycan_mono_emission = [glycan_mono_emission]
+            glycan_link_emission = glycan_test.get_linkage_emssions()
 
+            # calculate the likelihood
+            fwd_tree_sequence = fwd_seq_gen.forward_sequence_generator(glycan_sparse_matrix)
+
+            bind_fwd_probs = forward.forward(binding_hmm_trained, glycan_sparse_matrix, glycan_mono_emission, fwd_tree_sequence)
+            nonbind_fwd_probs = forward.forward(nonbinding_hmm_trained, glycan_sparse_matrix, glycan_mono_emission, fwd_tree_sequence)
+
+            if logsumexp(bind_fwd_probs.iloc[:, -1]) >= logsumexp(nonbind_fwd_probs.iloc[:, -1]):
+                testpred.append(1)
+            else:
+                testpred.append(0)
+
+        print(classification_report(testlabel, testpred))
+        cv_label += testlabel
+        cv_pred += testpred
+    return cv_label, cv_pred
 
 
 if __name__ == '__main__':
-    train()
+    y_label, y_pred = train(n_folds=10, max_iter=100)
+    print(classification_report(y_label, y_pred))
 
 
